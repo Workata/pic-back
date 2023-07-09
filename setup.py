0faf0730137aa906104c8c81bf7b3c5a99fe982f
Workata <jakubtolsciuk@gmail.com>
2023-07-09 12:50:08 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='app-name',
+    version='0.0.1',
+    author='author',
+    description='This is a template project',
+    author_email='author@gmail.com',
+    url='www.project-url.com',
+    keywords='template, example',
+    python_requires='>=3.11, <4',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
